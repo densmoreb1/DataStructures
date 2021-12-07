5c192e74d4ab31cac429129d90edb9aa1cfe259d
Brandon Densmore <78512643+densmoreb1@users.noreply.github.com>
2021-12-07 08:35:01 -0700

    added prove

--- linked_prove.py
+++ linked_prove.py
@@ -1,6 +1,11 @@
+""" 
+We will be taking names and keeping order of people who come into the DMV.
+They are able to take their own seat number but it is our job to take whoever is next in line.
+To do so we will need a linked list
+"""
+
 class LinkedList:
-    """ Constructing linked list object by creating the head and the tail 
-    """
+    """ Constructing linked list object by creating the head and the tail """
     class Node:
         """ A object within LinkedList to hold the value and the addresses of next and prev """
         def __init__(self, data):
@@ -12,24 +17,56 @@ class LinkedList:
         self.head = None
         self.tail = None
 
-    def insert_head(self, value):
-        """ Insert a node at the head """
-        # Create new node
-        new = LinkedList.Node(value)
-
-        # If the list is empty
+    def insert_front(self, value):
+        """
+        If someone has a very urgent appointment at the DMV they can be in the front.
+        """
+        # Create the new node
+        new_person = LinkedList.Node(value)  
+        
         if self.head is None:
-            self.head = new
-            self.tail = new
+            self.head = new_person
+            self.tail = new_person
         else:
-            new.next = self.head
-            self.head.prev = new
-            self.head = new
+            new_person.next = self.head # Connect new node to the previous head
+            self.head.prev = new_person # Connect the previous head to the new node
+            self.head = new_person      # Update the head to point to the new node
 
     ###################
     # Start Problem 1 #
     ###################
     def insert_tail(self, value):
-        """ Insert a node at the tail """
+        """
+        Insert a new node at the back (i.e. the tail) of the 
+        linked list.
+        """
+        # Create new node
+        new_node = LinkedList.Node(value)
+    
+    #################
+    # End Problem 1 #
+    #################
 
-        pass
+    def __iter__(self):
+        """
+        Iterate foward through the Linked List
+        """
+        curr = self.head  # Start at the begining since this is a forward iteration.
+        while curr is not None:
+            yield curr.data  # Provide (yield) each item to the user
+            curr = curr.next # Go forward in the linked list
+
+    def __str__(self):
+        """
+        Return a string representation of the linked list.
+        """
+        output = "linkedlist["
+        first = True
+        for value in self:
+            if first:
+                first = False
+            else:
+                output += ", "
+            output += str(value)
+        output += "]"
+        return output
